69d019c976cd76a9403907a765c17821a6245a5e
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-11-24 19:10:23 +0600

    initialized by mohammad

--- 01-node-tutorial/06-alternative-flavor.js
+++ 01-node-tutorial/06-alternative-flavor.js
@@ -1,6 +1,8 @@
-module.exports.items = ['item1', 'item2']
-const person = {
-  name: 'bob',
-}
+// const items = ['item1', 'item2']
+module.exports.items = ["item1", "item2"];
 
-module.exports.singlePerson = person
+const person = {
+    name: "bob",
+};
+
+module.exports.singlePerson = person;
--- 01-node-tutorial/07-mind-grenade.js
+++ 01-node-tutorial/07-mind-grenade.js
@@ -1,8 +1,11 @@
-const num1 = 5
-const num2 = 10
+const num1 = 5;
+const num2 = 10;
 
 function addValues() {
-  console.log(`the sum is : ${num1 + num2}`)
+    console.log(`the sum is : ${num1 + num2}`);
 }
 
-addValues()
+addValues(); // ei file e jehetu function ta call kora hoyeche ..
+// tai .. jei file e require kora hobe .. shei line e automatic call hoye jabe ...
+
+// require('./07-mind-grenade') mane .. ei ta likhlei call hoye jabe
